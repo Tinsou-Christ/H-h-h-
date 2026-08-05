2384312871ce343f323156bffff181986abde8aa
Tinsou-Christ <anosvoldigo291@gmail.com>
2026-08-05 17:09:12 +0000

    Update quiz.js

--- scripts/cmds/quiz.js
+++ scripts/cmds/quiz.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const BASE_URL = 'https://dockerfile-fixer.onrender.com/api';
+const BASE_URL = 'https://quiz-api-zd8a.onrender.com/api';
 
 module.exports = {
   config: {
